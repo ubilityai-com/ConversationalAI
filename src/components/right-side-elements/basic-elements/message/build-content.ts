863b2d89e646da0f72849f7e44c440525cd7bedc
Mostafa Deeb <mostafadeeb93000@gmail.com>
2025-08-19 17:26:30 +0300

    fix message not having next

--- src/components/right-side-elements/basic-elements/message/build-content.ts
+++ src/components/right-side-elements/basic-elements/message/build-content.ts
@@ -1,20 +1,20 @@
-import { stringifyAndExtractVariables } from "../../../../lib/utils";
+import { getNextNodeId, stringifyAndExtractVariables } from "../../../../lib/utils";
 import { RightSideData } from "./message";
 
-export default function getContent(node: {data:{rightSideData:RightSideData}}, params: any) {    
-    const data=node.data.rightSideData
-    
-    const content = {
-      type: "data",
-      data: {
-        text: data.botSays,
-      },
-    };
-    return {
-      type: "Message",
-      content: content,
-      // next: getNextNodeId(selectedNodeId, params.edges, params.nodes, null),
-      saveUserInputAs: data.save ? data.variableName : null,
-      usedVariables: stringifyAndExtractVariables(content),
-    };
-  }
+export default function getContent(node: { id: string, data: { rightSideData: RightSideData } }, params: any) {
+  const data = node.data.rightSideData
+
+  const content = {
+    type: "data",
+    data: {
+      text: data.botSays,
+    },
+  };
+  return {
+    type: "Message",
+    content: content,
+    next: getNextNodeId(node.id, params.edges, params.nodes, null),
+    saveUserInputAs: data.save ? data.variableName : null,
+    usedVariables: stringifyAndExtractVariables(content),
+  };
+}
