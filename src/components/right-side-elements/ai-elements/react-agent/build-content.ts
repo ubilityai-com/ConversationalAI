1f621c618323e6b6dd3d396a0c985eb9416bb175
Mostafa Deeb <mostafadeeb93000@gmail.com>
2025-09-02 15:29:08 +0300

    fix in RA

--- src/components/right-side-elements/ai-elements/react-agent/build-content.ts
+++ src/components/right-side-elements/ai-elements/react-agent/build-content.ts
@@ -5,7 +5,6 @@ import { extractCreds, getNextNodeId, stringifyAndExtractVariables } from "../..
 export default function getContent(selectedNode: any, params: any) {
     const rightSideData = selectedNode.data.rightSideData
     const model = rightSideData.extras.model
-    const memory = rightSideData.extras.memory
     const tool = rightSideData.extras.tool
     const { edges, nodes } = params
     const toolConfigs = tool.list.length !== 0
@@ -26,7 +25,6 @@ export default function getContent(selectedNode: any, params: any) {
         data: {
             inputs: { query: rightSideData.json.query, prompt: getAccvalue(rightSideData.json, "prompt") },
             model: require("../../../properties/contents/model")[model.type](selectedNode),
-            chainMemory: require("../../../properties/contents/memory")[memory.type](selectedNode),
             cred: extractCreds(selectedNode?.data.rightSideData.extras),
             tools: toolConfigs.length > 0 || selectedTools.length > 0 ?
                 {
